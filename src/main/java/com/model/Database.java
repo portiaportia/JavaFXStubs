53ce5f8afd4f35707f557c40ae185f6aa012b252
portiaportia <portiaplante@gmail.com>
2024-12-17 12:30:41 -0500

    updated for stubs

---
+++ src/main/java/com/model/Database.java
@@ -0,0 +1,15 @@
+package com.model;
+
+
+import java.util.ArrayList;
+
+public class Database {
+	
+	public static ArrayList<User> getUsers(){
+		return new ArrayList<User>();
+	}
+	
+	public static ArrayList<Item> getItems(){
+		return new ArrayList<Item>();
+	}
+}
